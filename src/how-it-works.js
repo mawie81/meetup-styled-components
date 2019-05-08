334ba7510f1360726cdea0209daf76e61a4cdd67
Marcel Wiehle <mawieh@googlemail.com>
2019-05-08 15:35:52 +0200

    add small demo of this all works

---
+++ src/how-it-works.js
@@ -0,0 +1,17 @@
+const log = (...args) => console.log(...args);
+
+//log`1`
+
+//const interpolation = "test";
+//log`12 ${interpolation}`
+//log`test ${(arg) => arg ? true : false}`
+
+// const execFuncArgs = (...args) => args.forEach(arg => {
+//   if (typeof arg === 'function') {
+//     arg()
+//   }
+// });
+
+// execFuncArgs("test")
+// execFuncArgs`test`
+// execFuncArgs`test ${() => console.log('executed')}`
