ded42de6d5f6a536023d18f623bf428aede8b843
Marcel Wiehle <mawieh@googlemail.com>
2019-05-08 14:57:51 +0200

    update update update

--- src/intro/1 - basic.js
+++ src/intro/1 - basic.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Button = styled.button`
   padding: 6px 12px;
   color: white;
   font-size: 18px;
   background-color: ${props => (props.primary ? "#00b2a5" : "#ffa64c")};
+  ${props =>
+    props.primary &&
+    css`
+      border: 2px solid darkblue;
+    `}
 `;
 
 function App({ className }) {
